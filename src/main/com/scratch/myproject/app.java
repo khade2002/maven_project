3ceaa4eae4b34f7850f402cbae5fb93732ab7f4e
Prasad <khade1676@gamil.com>
2025-04-29 07:12:45 +0000

    push to maven project

---
+++ src/main/com/scratch/myproject/app.java
@@ -0,0 +1,19 @@
+package com.scratch.myproject;
+
+/**
+ * Hello world!
+ */
+public class app {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public app() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
